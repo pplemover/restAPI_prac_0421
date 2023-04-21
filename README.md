0d2f3d080087e5ec0c98113986513f44678ad567
paulimbest <paulimbest@naver.com>
2023-04-21 09:47:40 +0900

    save

---
+++ README.md
@@ -0,0 +1,9 @@
+# DB 설계를 활용한 REST API 설계
+
+### 목표
+- DRF(Django Rest Framework)를 활용한 API server 제작
+- Database many to one relationship(1:N)에 대한 이해
+- Database many to many relationship(M:N)에 대한 이해
+  
+### Process Log
+(1) 주어진 fixtures 파일은 movies 앱 
---
+++ movies/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Actor(models.Model):
+    name = models.CharField(max_length=100)
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    overview = models.TextField()
+    release_date = models.DateTimeField()
+    poster_path = models.TextField()
+    
+class Review(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    
+
